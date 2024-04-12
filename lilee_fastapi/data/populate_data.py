767305caf98e1e5dbb4fcf586c32744ccfc8dbad
Haw Yuan <hawyuan1lv@gmail.com>
2024-04-13 02:23:35 +0800

    deactivate the 20th group only

--- lilee_fastapi/data/populate_data.py
+++ lilee_fastapi/data/populate_data.py
@@ -15,44 +15,40 @@ from src.repositories.models.users_model import Users
 from src.repositories.database import SessionLocal
 
 
-def populate_data(db, data):
-    creators = ["root", "carsten"]
+def populate_data(db, data) -> None:
+    creators = ["Admin", "Jay Chou", "Eason Chen", "Carsten Yuan"]
 
     for group in data["groups"]:
         new_group = Groups(name=group, creator=random.choice(creators))
         db.add(new_group)
     db.commit()
 
-    groups = db.query(Groups).all()
-    assign_none = [random.randint(0, len(data["users"]) - 1) for _ in range(30)]
+    # deactivated the group Meditation
+    group = db_session.query(Groups).filter(Groups.id == 20).one_or_none()
+    group.is_activate = False
+    group.modifier = random.choice(
+        ["Alice", "Bob", "Charlie", "David", "Eve"]
+    )  # Mock modifiers
+    db.commit()
+
+    # add users and assign a group or none to them
+    groups = db_session.query(Groups).filter(Groups.is_activate == 1).all()
+    assign_none = [random.randint(0, len(data["users"]) - 1) for _ in range(20)]
 
     for i, user in enumerate(data["users"]):
-        if i in assign_none:
-            group = None  # None == don't join any groups
-        else:
-            group = random.choice(groups)
+        group = None if i in assign_none else random.choice(groups)
         new_user = Users(name=user, group=group, creator=random.choice(creators))
         db.add(new_user)
     db.commit()
-    
-    starter = len(data["groups"])
-    for i, de_group in enumerate(data["deactivated_groups"]):
-        new_group = Groups(name=de_group, creator=random.choice(creators))
-        db.add(new_group)
-        # deactivate a group
-        group = db_session.query(Groups).filter(Groups.id == starter+i+1).one_or_none()
-        group.is_activate = False
-        group.modifier = random.choice(["Alice", "Bob", "Charlie", "David", "Eve"])  # Mock modifiers
-        db.commit()
 
     print("Successfully populated data!")
     db.close()
 
 
 if __name__ == "__main__":
-    with open("data/example_data.json", "r") as f:
-        data = json.load(f)
+    with open("data/example_data.json", "r", encoding="utf-8") as f:
+        fake_data = json.load(f)
 
     db_session = SessionLocal()
-    populate_data(db_session, data)
+    populate_data(db_session, fake_data)
     db_session.close()
