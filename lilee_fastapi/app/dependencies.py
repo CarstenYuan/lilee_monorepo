03798da786f3bc3f22e74669e8f71e5e6ef8e576
Haw Yuan <hawyuan1lv@gmail.com>
2024-04-12 19:35:50 +0800

    revise import path

--- lilee_fastapi/app/dependencies.py
+++ lilee_fastapi/app/dependencies.py
@@ -1,8 +1,8 @@
 from fastapi import Depends
 from sqlalchemy.orm import Session
-from app.repositories.database import get_db
-from app.repositories.user_repository import UserRepository
-from app.services.user_service import UserService
+from repositories.database import get_db
+from repositories.user_repository import UserRepository
+from services.user_service import UserService
 
 def get_user_service(db: Session = Depends(get_db)) -> UserService:
     user_repository = UserRepository(db)
