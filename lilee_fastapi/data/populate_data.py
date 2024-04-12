3e698871d702aa1d41e6b00fc65efe043c97cd2b
Haw Yuan <hawyuan1lv@gmail.com>
2024-04-13 01:05:43 +0800

    import os and sys for correct import path, add deactivate groups for better testing scenarios

--- lilee_fastapi/data/populate_data.py
+++ lilee_fastapi/data/populate_data.py
@@ -1,22 +1,32 @@
+import os
+import sys
 import json
 import random
-from models import Base, Users, Groups
-from lilee_fastapi.app.repositories.database import MySQLDB
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+
+from src.repositories.models.base import Base
+from src.repositories.models.groups_model import Groups
+from src.repositories.models.users_model import Users
+from src.repositories.database import SessionLocal
 
 
 def populate_data(db, data):
-    creators = ['root', 'carsten']
+    creators = ["root", "carsten"]
 
-    for group in data['groups']:
+    for group in data["groups"]:
         new_group = Groups(name=group, creator=random.choice(creators))
         db.add(new_group)
     db.commit()
 
     groups = db.query(Groups).all()
-    
-    assign_none = [random.randint(0, len(data)-1) for _ in range(30)]
-    
-    for i, user in enumerate(data['users']):
+    assign_none = [random.randint(0, len(data["users"]) - 1) for _ in range(30)]
+
+    for i, user in enumerate(data["users"]):
         if i in assign_none:
             group = None  # None == don't join any groups
         else:
@@ -24,20 +34,25 @@ def populate_data(db, data):
         new_user = Users(name=user, group=group, creator=random.choice(creators))
         db.add(new_user)
     db.commit()
+    
+    starter = len(data["groups"])
+    for i, de_group in enumerate(data["deactivated_groups"]):
+        new_group = Groups(name=de_group, creator=random.choice(creators))
+        db.add(new_group)
+        # deactivate a group
+        group = db_session.query(Groups).filter(Groups.id == starter+i+1).one_or_none()
+        group.is_activate = False
+        group.modifier = random.choice(["Alice", "Bob", "Charlie", "David", "Eve"])  # Mock modifiers
+        db.commit()
 
     print("Successfully populated data!")
     db.close()
 
 
 if __name__ == "__main__":
-    with open('example_data.json', 'r') as f:
+    with open("data/example_data.json", "r") as f:
         data = json.load(f)
 
-    db_manager = MySQLDB()
-    db_session = db_manager.SessionLocal()
-
-    try:
-        Base.metadata.create_all(bind=db_manager.engine)  # check if tables exist
-        populate_data(db_session, data)
-    finally:
-        db_session.close()
+    db_session = SessionLocal()
+    populate_data(db_session, data)
+    db_session.close()
