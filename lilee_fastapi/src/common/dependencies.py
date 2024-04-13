9293122ba754f10922fe2ea44ab27406197e2149
Haw Yuan <hawyuan1lv@gmail.com>
2024-04-13 23:43:21 +0800

    rename get_db to get_session

--- lilee_fastapi/src/common/dependencies.py
+++ lilee_fastapi/src/common/dependencies.py
@@ -1,15 +1,15 @@
 from fastapi import Depends
 from sqlalchemy.orm import Session
-from common.database import get_db
+from common.database import get_session
 from repositories.users import UserRepository
 from repositories.groups import GroupRepository
 from services.users import UserService
 from services.groups import GroupService
 
-def get_user_service(db: Session = Depends(get_db)) -> UserService:
+def get_user_service(db: Session = Depends(get_session)) -> UserService:
     user_repository = UserRepository(db)
     return UserService(user_repository)
 
-def get_group_service(db: Session = Depends(get_db)) -> GroupService:
+def get_group_service(db: Session = Depends(get_session)) -> GroupService:
     group_repository = GroupRepository(db)
     return GroupService(group_repository)
