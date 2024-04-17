b70b530535df8650f5ff5d0650a058b3e7342c76
Haw Yuan <hawyuan1lv@gmail.com>
2024-04-17 12:03:18 +0800

    fix pylint problems

--- lilee_fastapi/src/common/database.py
+++ lilee_fastapi/src/common/database.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# engine_url = "mysql+pymysql://root:1qaz2wsx@ABCDEFGHIJKLMNOPQRSTUVWXYZ:3306/lilee"  # to disconnect db
-# engine_url = "mysql+pymysql://root:1qaz2wsx@localhost:3306/lilee"  # for local tests
-engine_url = "mysql+pymysql://root:1qaz2wsx@mysqlDB:3306/lilee"
+# ENGINE_URL = "mysql+pymysql://root:1qaz2wsx@ABCDEFGHIJKLMNOPQRSTUVWXYZ:3306/lilee"
+# ENGINE_URL = "mysql+pymysql://root:1qaz2wsx@localhost:3306/lilee"  # for local tests
+ENGINE_URL = "mysql+pymysql://root:1qaz2wsx@mysqlDB:3306/lilee"
 
 
-engine = create_engine(engine_url)
+engine = create_engine(ENGINE_URL)
 SessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine, expire_on_commit=False
 )
