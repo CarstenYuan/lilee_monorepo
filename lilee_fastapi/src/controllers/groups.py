36d972936e005cf8b6873e5524b0b7c25cffcff4
Haw Yuan <hawyuan1lv@gmail.com>
2024-04-17 12:12:25 +0800

    fix pylint import-error, redefined-builtin problems

--- lilee_fastapi/src/controllers/groups.py
+++ lilee_fastapi/src/controllers/groups.py
@@ -1,9 +1,9 @@
+from typing import Optional
 from fastapi import APIRouter, Depends, Body
 from pydantic import BaseModel, Field
-from typing import Optional
 
-from services.groups import GroupService
-from common.dependencies import get_group_service
+from ..services.groups import GroupService
+from ..common.dependencies import get_group_service
 
 
 groups_statistic_router = APIRouter()
@@ -32,13 +32,13 @@ def add_group(
 
 
 @groups_statistic_router.delete("/deleteGroup/{id}", tags=groups_tag)
-def delete_group(id: int, service: GroupService = Depends(get_group_service)):
-    return service.delete_group(id)
+def delete_group(group_id: int, service: GroupService = Depends(get_group_service)):
+    return service.delete_group(group_id)
 
 
 @groups_statistic_router.get("/getSingleGroup/{id}", tags=groups_tag)
-def get_single_group(id: int, service: GroupService = Depends(get_group_service)):
-    return service.get_single_group(id)
+def get_single_group(group_id: int, service: GroupService = Depends(get_group_service)):
+    return service.get_single_group(group_id)
 
 
 @groups_statistic_router.get("/getAllGroups", tags=groups_tag)
@@ -54,8 +54,8 @@ def get_active_groups(service: GroupService = Depends(get_group_service)):
 # TODO: modify api route here and vue's
 @groups_statistic_router.put("/updateGroupInfo/{id}", tags=groups_tag)
 def update_group(
-    id: int,
+    group_id: int,
     update_request: UpdateGroupRequest = Body(...),
     service: GroupService = Depends(get_group_service),
 ):
-    return service.update_group(id, update_request)
+    return service.update_group(group_id, update_request)
